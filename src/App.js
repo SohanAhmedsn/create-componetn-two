299b7c99888f0e8de3c9abd06f3f55ddbad868b6
sn77sohan <sohan.ahmedself@gmail.com>
2021-11-07 13:57:12 +0600

    world-icc-player

--- src/App.js
+++ src/App.js
@@ -4,22 +4,29 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <Player name="Joe Root" country="England"></Player>
+    <Player name="Kane Williamson" country="NZ"></Player>
+    <Player name="Steve Smith" country="Aus"></Player>
+    <Player name="Marnus Labuschagne" country="AUS"></Player>
+    <Player name="Rohit Sharma" country="IND"></Player>
+    <Player name="Virat Kohli" country="IND"></Player>
+    <Player name="Babar Azam" country="PAK"></Player>
+    <Player name="David Warner" country="AUS"></Player>
+    <Player name="Quinton de Kock" country="SA"></Player>
+    <Player name="Henry Nicholls" country="NZ"></Player>
+    <Player name="Dimuth Karunaratne" country="SL"></Player>
     </div>
   );
+}
+function Player(props){
+  console.log(props);
+  return (
+    <div className="player">
+      <h1>Name: {props.name} </h1>
+      <h2>Country: {props.country} </h2>
+
+    </div>
+  )
 }
 
 export default App;
